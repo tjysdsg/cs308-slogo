1a25f46e1d1d2f1849993e9d71907f92cda0c57d
Jiyang Tang <jt304@duke.edu>
2021-03-02 09:01:32 +0800

    Add 3 more use cases

--- doc/plan/usecases/usecases-jiyang.java
+++ doc/plan/usecases/usecases-jiyang.java
@@ -1,4 +1,6 @@
 /**
+ * Use case 1:
+ * <p>
  * The user types 'fd 50' in the command window, and sees the turtle move in the display window
  * leaving a trail, and the command is added to the environment's history.
  */
@@ -17,10 +19,52 @@ class Environement {
 
   public double runCommand(String cmd) {
     AST ast = parser.parseCommand(cmd);
-    double ret = ast.evaluate(executionBundle);
-    this.notifyTurtleUpdate(turtleInfo);
-    this.notifyCommandUpdate(commandInfo);
-    this.notifyVariableUpdate(variableInfo);
-    return ret;
+    return ast.evaluate(executionBundle);
   }
 }
+
+  /**
+   * Use case 2:
+   * <p>
+   * The user click on an item in the command table, and a help dialog pops up
+   */
+
+// after receiving the selected command, View does this:
+Command cmd = modelController.getCommand(cmd);
+this.showCommandHelp(Command cmd);
+
+class ModelController {
+
+  public double getCommand(String name) {
+    return infoBundle.getLookupTable().get(name);
+  }
+}
+
+/**
+ * Use case 3:
+ * <p>
+ * The user changes the language of the application
+ */
+
+// view calls this
+viewController.setLanguage(language);
+modelController.setLanguage(language);
+
+/**
+ * Use case 4:
+ * <p>
+ * The user types 'CLEARSCREEN' command, and turtle's trails is erased and it is sent to the home
+ * position, returns distance turtle moved
+ */
+
+// Same as use case 1, except in ClearScreen#evaluate:
+class ClearScreenCmd extends AST {
+
+  public double evaluate(ExecutionBundle executionBundle) {
+    Turtle turtle = executionBundle.getTurtle();
+    Vec2D prevPos = turtle.getPos();
+    turtle.setTransform(0.0, 0.0, 0.0);
+    return prevPos.magnitude(new Vec2D(0.0, 0.0));
+  }
+}
+

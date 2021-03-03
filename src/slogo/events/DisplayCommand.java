f0e5a0b63cb6218522c4bb92ee83b64c5f5c7808
joshu <joshuapetitma@yahoo.com>
2021-03-03 17:16:05 -0500

    Add final fields corresponding to field names
    These final fields allow the view to maintain an updated
    reference to field names. Instead of hardcoding them in

--- src/slogo/events/DisplayCommand.java
+++ src/slogo/events/DisplayCommand.java
@@ -4,6 +4,9 @@ package slogo.events;
  * The state of a command within the environment at a specific instance.
  */
 public record DisplayCommand(String name,String signature){
+  public static final String NAME = "name";
+  public static final String SIGNATURE = "value";
+
   public String getName() {
     return name;
   }
--- src/slogo/events/DisplayVariable.java
+++ src/slogo/events/DisplayVariable.java
@@ -4,6 +4,9 @@ package slogo.events;
  * Displayable variables are used to communicate the value of a variable within the environment.
  */
 public record DisplayVariable(String name,String value){
+  public static final String NAME = "name";
+  public static final String VALUE = "value";
+
   public String getName() {
     return name;
   }

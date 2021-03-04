8ea17c182e9e77ce2f0777dd5e054e01cda04774
Jiyang Tang <jt304@duke.edu>
2021-03-04 10:33:00 +0800

    Implement home and clearscreen (partly)

---
+++ src/slogo/model/ASTNodes/ASTClearScreen.java
@@ -0,0 +1,25 @@
+package slogo.model.ASTNodes;
+
+import slogo.model.InfoBundle;
+import slogo.model.Turtle;
+import slogo.model.Vec2D;
+
+public class ASTClearScreen extends ASTCommand {
+
+  private static final int NUM_PARAMS = 0;
+  private static final String NAME = "clearscreen";
+
+  public ASTClearScreen() {
+    super(NAME, NUM_PARAMS);
+  }
+
+  @Override
+  protected double doEvaluate(InfoBundle info) {
+    // TODO: erase trails
+
+    Turtle turtle = info.getTurtle();
+    Vec2D src = new Vec2D(turtle.getX(), turtle.getY());
+    turtle.setPosition(0, 0);
+    return (new Vec2D(0, 0)).minus(src).magnitude();
+  }
+}

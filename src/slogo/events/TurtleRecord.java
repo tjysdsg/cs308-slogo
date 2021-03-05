67dd7b502e47ef9c4aaa21d9fdc58843cef79708
joshu <joshuapetitma@yahoo.com>
2021-03-04 20:44:48 -0500

    adds previous values to turtle records

--- src/slogo/events/TurtleRecord.java
+++ src/slogo/events/TurtleRecord.java
@@ -3,6 +3,6 @@ package slogo.events;
 /**
  * The state of a turtle at a specific instance.
  */
-public record TurtleRecord(int id, double xCoord, double yCoord, double rotation) {
+public record TurtleRecord(int id, double prevX, double prevY, double prevRot, double xCoord, double yCoord, double rotation) {
 
 }

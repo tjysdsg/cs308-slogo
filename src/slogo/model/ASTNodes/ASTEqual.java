ed5b458456d3123327dbb6a66d02137a1df54478
Jiyang Tang <jt304@duke.edu>
2021-03-05 19:14:01 +0800

    Implement boolean operators

---
+++ src/slogo/model/ASTNodes/ASTEqual.java
@@ -0,0 +1,15 @@
+package slogo.model.ASTNodes;
+
+public class ASTEqual extends ASTBinaryOperator {
+
+  private static final String NAME = "equal?";
+
+  public ASTEqual() {
+    super(NAME);
+  }
+
+  @Override
+  protected double calculate(double ret1, double ret2) {
+    return ret1 == ret2 ? 1.0 : 0.0;
+  }
+}

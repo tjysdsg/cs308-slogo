92b6e5175d81ab704900af0e130106dc357b70f8
joshu <joshuapetitma@yahoo.com>
2021-03-05 07:10:21 -0500

    Moves turtles and animation to separate class

---
+++ src/slogo/view/Turtle.java
@@ -0,0 +1,92 @@
+package slogo.view;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.Queue;
+import javafx.animation.Animation;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
+import javafx.scene.image.Image;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Line;
+import javafx.util.Duration;
+import slogo.events.TurtleRecord;
+import javafx.application.Platform;
+
+public class Turtle extends ImageView {
+  private double currX;
+  public static double ANIMATION_SPEED = 200;
+  private double currY;
+  private double rotation;
+  private Queue<Animation> animationQueue;
+
+  protected Turtle(Image image) {
+    super(image);
+    this.currX = 0;
+    this.currY = 0;
+    this.animationQueue = new LinkedList<>();
+    setOnMouseClicked(
+        e -> {
+          System.out.println("YAA!");
+        });
+  }
+
+  public Turtle() {
+    this(new Image(new File("data/images/logo.png").toURI().toString()));
+  }
+
+  public double getCurrX() {
+    return this.currX;
+  }
+
+  public double getCurrRot() {
+    return this.rotation;
+  }
+
+  public double getCurrY() {
+    return this.currY;
+  }
+
+  public void update(TurtleRecord info) {
+    if (getCurrRot() != info.rotation()) {
+      RotateTransition rt = new RotateTransition(Duration.millis(ANIMATION_SPEED), this);
+      rt.setByAngle(getCurrRot() - info.rotation());
+      this.rotation = info.rotation();
+      addAnimation(rt);
+    }
+    double tx = getCurrX();
+    double ty = getCurrY();
+    if (tx != info.xCoord() || ty != info.yCoord()) {
+      TranslateTransition moveTurtle = new TranslateTransition();
+      moveTurtle.setDuration(Duration.millis(ANIMATION_SPEED));
+      if (tx != info.xCoord()) {
+        moveTurtle.setToX(info.xCoord());
+      } 
+      if (ty != info.yCoord()) {
+        moveTurtle.setToY(-info.yCoord());
+      }
+      moveTurtle.setNode(this);
+      addAnimation(moveTurtle);
+      this.currX = info.xCoord();
+      this.currY = info.yCoord();
+    }
+  }
+
+  public void addAnimation(Animation an) {
+    animationQueue.add(an);
+    an.setOnFinished(
+        e -> {
+          if (animationQueue.peek() == an) {
+            animationQueue.poll();
+          }
+          if (animationQueue.size() > 0) {
+            animationQueue.poll().play();
+          }
+        });
+    if (animationQueue.size() == 1) {
+      an.play();
+    }
+  }
+}

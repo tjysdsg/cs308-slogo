aa5b392e9173702b7a3c8faa5d9d51653f7ad318
Jiyang Tang <jt304@duke.edu>
2021-03-07 11:43:43 +0800

    Implement ASTIf and ASTIfElse

---
+++ src/slogo/model/ASTNodes/ASTIfElse.java
@@ -0,0 +1,29 @@
+package slogo.model.ASTNodes;
+
+import slogo.model.InfoBundle;
+
+/**
+ * Children:
+ * <ol>
+ *   <li>Predicate</li>
+ *   <li>Then branch</li>
+ *   <li>Else branch</li>
+ * </ol>
+ */
+public class ASTIfElse extends ASTCommand {
+
+  private static final int NUM_PARAMS = 3;
+  private static final String NAME = "ifelse";
+
+  public ASTIfElse() {
+    super(NAME, NUM_PARAMS);
+  }
+
+  @Override
+  protected double doEvaluate(InfoBundle info) {
+    if (getChildAt(0).evaluate(info) != 0) {
+      return getChildAt(1).evaluate(info);
+    }
+    return getChildAt(2).evaluate(info);
+  }
+}

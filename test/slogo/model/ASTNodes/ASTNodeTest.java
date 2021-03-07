487c226b4a6533e1e0f3c7f035032389520a7def
Jiyang Tang <jt304@duke.edu>
2021-03-07 22:55:13 +0800

    Add tests for evaluation of move commands, fix ASTBack

---
+++ test/slogo/model/ASTNodes/ASTNodeTest.java
@@ -0,0 +1,107 @@
+package slogo.model.ASTNodes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import slogo.events.CommandsRecord;
+import slogo.events.TurtleRecord;
+import slogo.events.VariablesRecord;
+import slogo.model.ASTNodes.ASTNode;
+import slogo.model.InfoBundle;
+import slogo.model.Turtle;
+import slogo.model.parser.CommandClassifier;
+import slogo.model.parser.Parser;
+import slogo.model.parser.ProgramParser;
+
+/**
+ * Test evaluation of ASTNodes
+ * <p>
+ * For convenience, this test uses parser to build command node tree, so parser tests must pass
+ * first
+ */
+public class ASTNodeTest {
+
+  private Parser parser;
+  private TestBundle infoBundle;
+
+  @BeforeEach
+  void setUp() {
+    parser = new ProgramParser("English");
+    infoBundle = new TestBundle();
+  }
+
+  double parseAndEvaluateCommands(String cmd, double param) {
+    String str = String.format("%s %f", cmd, param);
+    ASTNode node = parser.parseCommand(str);
+    return node.evaluate(infoBundle);
+  }
+
+  @Test
+  void testMoveCommands() {
+    Random rand = new Random();
+    for (String cmd : new String[]{
+        "FORWARD",
+        "BACK",
+        "RIGHT",
+        "LEFT",
+    }) {
+      infoBundle = new TestBundle(); // reset turtle
+
+      double a = rand.nextDouble() * 200.0 - 100.0;
+      double res = parseAndEvaluateCommands(cmd, a);
+      assertEquals(a, res, 1E-5);
+
+      switch (cmd) {
+        case "FORWARD" -> assertEquals(a, infoBundle.getTurtle().getY(), 1E-5);
+        case "BACK" -> assertEquals(-a, infoBundle.getTurtle().getY(), 1E-5);
+        case "RIGHT" -> assertEquals(a, infoBundle.getTurtle().getRotation(), 1E-5);
+        case "LEFT" -> assertEquals(-a, infoBundle.getTurtle().getRotation(), 1E-5);
+      }
+    }
+  }
+
+  class TestBundle implements InfoBundle {
+
+    private Map<String, ASTNode> lookupTable;
+    private TurtleRecord info;
+    private Turtle turtle;
+
+    public TestBundle() {
+      lookupTable = new HashMap<>();
+      info = new TurtleRecord(0, 0, 0, 0, true, true);
+      turtle = new Turtle(0, this);
+    }
+
+    @Override
+    public Turtle getTurtle() {
+      return turtle;
+    }
+
+    @Override
+    public void notifyTurtleUpdate(TurtleRecord info) {
+      this.info = info;
+    }
+
+    @Override
+    public void notifyCommandUpdate(CommandsRecord info) {
+    }
+
+    @Override
+    public void notifyVariableUpdate(VariablesRecord info) {
+    }
+
+    @Override
+    public Map<String, ASTNode> getLookupTable() {
+      return lookupTable;
+    }
+
+    public TurtleRecord getInfo() {
+      return info;
+    }
+  }
+}

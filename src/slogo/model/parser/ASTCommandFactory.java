11e570658e76bf6eeee089f74dcfce6d3bfb6674
Oliver Rodas <oliver.rodas@duke.edu>
2021-03-07 20:17:35 -0500

    JavaDoc for ASTCommandFactory

--- src/slogo/model/parser/ASTCommandFactory.java
+++ src/slogo/model/parser/ASTCommandFactory.java
@@ -5,11 +5,26 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import slogo.model.ASTNodes.ASTCommand;
 
+/**
+ * The ASTCommand factory class is used to convert a string into an ASTNode using reflection.
+ *
+ * This class assumes that every ASTNode class is inside of the ASTNodes package. Every subclass of ASTNode must contain the NAME static field in order to be found.
+ *
+ * This class depends on the ASTCommand class, but only for finding its children. It depends on the java reflection package and the file class.
+ *
+ * This class is used by simply calling getCommand on the class itself to get the ASTNode corresponding to the input string.
+ *
+ * @author Oliver Rodas
+ */
 public class ASTCommandFactory {
-  // FIXME: Find better way of referencing the root package of ASTNode
   public static final String packagePath = "slogo.model.ASTNodes.";
   public static final String dirPath = "src/" + packagePath.replace(".", "/") + "/";
 
+  /**
+   * Get the ASTNode corresponding to the input string
+   * @param command The command to find the node for
+   * @return The command if found, null if not
+   */
   public static ASTCommand getCommand(String command) {
     Class[] availNodes = getASTNodes();
     for (Class node : availNodes) {
@@ -38,7 +53,7 @@ public class ASTCommandFactory {
     return null;
   }
 
-  public static Class[] getASTNodes() {
+  private static Class[] getASTNodes() {
     File myPackage = new File(dirPath);
     assert(myPackage.isDirectory());
 
@@ -57,7 +72,7 @@ public class ASTCommandFactory {
     return nodes;
   }
 
-  public static boolean isNodeDescendantOfClass(Class node, Class ancestor) {
+  private static boolean isNodeDescendantOfClass(Class node, Class ancestor) {
     Class parent = node.getSuperclass();
     if (parent == null)
       return false;
@@ -66,7 +81,7 @@ public class ASTCommandFactory {
     return isNodeDescendantOfClass(parent, ancestor);
   }
 
-  public static boolean isNodeAbstract(Class node) {
+  private static boolean isNodeAbstract(Class node) {
     return Modifier.isAbstract(node.getModifiers());
   }
 }

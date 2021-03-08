c43136b30d1a458061ca4ba0e1a9c811d655720a
Oliver Rodas <oliver.rodas@duke.edu>
2021-03-07 20:35:14 -0500

    JavaDoc for CommandClassifier

--- src/slogo/model/parser/ASTCommandFactory.java
+++ src/slogo/model/parser/ASTCommandFactory.java
@@ -7,13 +7,15 @@ import slogo.model.ASTNodes.ASTCommand;
 
 /**
  * The ASTCommand factory class is used to convert a string into an ASTNode using reflection.
- *
+ *<p>
  * This class assumes that every ASTNode class is inside of the ASTNodes package. Every subclass of ASTNode must contain the NAME static field in order to be found.
- *
+ *</p>
+ * <p>
  * This class depends on the ASTCommand class, but only for finding its children. It depends on the java reflection package and the file class.
- *
+ * </p>
+ * <p>
  * This class is used by simply calling getCommand on the class itself to get the ASTNode corresponding to the input string.
- *
+ *</p>
  * @author Oliver Rodas
  */
 public class ASTCommandFactory {

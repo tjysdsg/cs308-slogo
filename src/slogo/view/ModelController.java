ddb8306cad33cec11dd4314af7fefc2494326c69
joshu <joshuapetitma@yahoo.com>
2021-03-09 08:26:15 -0500

    Add Alert handling for exceptions

--- src/slogo/view/ModelController.java
+++ src/slogo/view/ModelController.java
@@ -20,10 +20,11 @@ import slogo.exceptions.ModelException;
  */
 
 public class ModelController {
-  ViewController vc;
-  TrackableEnvironment env;
-  ResourceBundle exceptions;
-  ResourceBundle translation;
+  private static final String RESOURCES_PACKAGE = "resources.commands.";
+  private ViewController vc;
+  private TrackableEnvironment env;
+  private ResourceBundle exceptions;
+  private ResourceBundle translation;
 
   /**
    * This is the constryctor for the model controller class
@@ -31,6 +32,7 @@ public class ModelController {
    * specific obkects. It is an empty method
    */
   public ModelController() {
+    exceptions = ResourceBundle.getBundle(RESOURCES_PACKAGE + "exceptions.English");
   }
 
   public void setController(ViewController vc) {
@@ -62,10 +64,9 @@ public class ModelController {
       try {
         env.runCommand(command);
       } catch(ModelException e) {
-        System.out.println("Error:");
-        System.out.println(e);
-        //String excp = buildException(e);
-        //vc.sendAlert("Error", excp);
+        System.out.println("Error: " + e.toString());
+        String message = e.buildException(exceptions.getString(e.getMessage()));
+        vc.sendAlert("Error", message);
       }
     } else {
 
@@ -79,9 +80,5 @@ public class ModelController {
    */
   public void setLanguage(String language) {
 
-  }
-
-  private String buildException(ModelException e) {
-    return e.buildException(exceptions.getString(e.getMessage()));
   }
 }

e20ea6f417fc009ffecd32d1ab75a98689ba6d1a
Jiyang Tang <jt304@duke.edu>
2021-03-16 09:19:02 +0800

    Add ASTTurtleCommand

---
+++ src/slogo/model/ASTNodes/ASTTurtleCommand.java
@@ -0,0 +1,30 @@
+package slogo.model.ASTNodes;
+
+import java.util.ArrayList;
+import java.util.List;
+import slogo.model.InfoBundle;
+import slogo.model.Turtle;
+
+public abstract class ASTTurtleCommand extends ASTCommand {
+
+  public ASTTurtleCommand(String name, int numParams) {
+    super(name, numParams);
+  }
+
+  @Override
+  protected final double doEvaluate(InfoBundle info) {
+    ArrayList<Double> params = new ArrayList<>();
+    for (int i = 0; i < getNumParams(); ++i) {
+      params.add(getChildAt(i).evaluate(info));
+    }
+
+    double ret = 0;
+    List<Turtle> turtles = info.getActiveTurtles();
+    for (Turtle t : turtles) {
+      ret = evaluateForTurtle(t, params);
+    }
+    return ret;
+  }
+
+  protected abstract double evaluateForTurtle(Turtle turtle, List<Double> parameters);
+}

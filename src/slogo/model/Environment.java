e3c39562dc96f807723acd47574f28d3f661594a
Oliver Rodas <oliver.rodas@duke.edu>
2021-03-16 21:41:50 -0400

    Cleaning up the Environment class

--- src/slogo/model/Environment.java
+++ src/slogo/model/Environment.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.ResourceBundle;
 import slogo.events.ClearEnvironment;
 import slogo.events.CommandsRecord;
 import slogo.events.TurtleRecord;

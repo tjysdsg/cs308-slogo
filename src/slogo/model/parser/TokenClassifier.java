ff3350fc4da69423f22a61ef57b9695060c41fdc
Oliver Rodas <oliver.rodas@duke.edu>
2021-03-18 01:43:36 -0400

    Removed TokenClassifier.java

--- src/slogo/model/parser/TokenClassifier.java
+++
@@ -1,8 +0,0 @@
-package slogo.model.parser;
-
-//public class TokenClassifier extends TokenParser implements SyntaxClassifier {
-//  private static final String BUNDLE = "Syntax";
-//  public TokenClassifier() {
-//    addPatterns(BUNDLE);
-//  }
-//}

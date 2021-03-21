00a070b88ac8913337484abdfc0b4e19ff5f869e
Oliver Rodas <oliver.rodas@duke.edu>
2021-03-20 22:53:57 -0400

    Created a record to send data to the handlers

---
+++ src/slogo/model/parser/handlers/ParserRecord.java
@@ -0,0 +1,11 @@
+package slogo.model.parser.handlers;
+
+import java.util.List;
+import java.util.Stack;
+import slogo.model.InfoBundle;
+import slogo.model.parser.Scope;
+
+public record ParserRecord(Stack<Scope> scopeStack, List<String> tokensLeft,
+                           String language, InfoBundle environmentInfo,
+                           String currCommand) {
+}

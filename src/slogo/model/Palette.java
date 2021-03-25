ef0d53c07504374b085c850a26b92982570bff19
Jiyang Tang <jt304@duke.edu>
2021-03-25 13:01:41 +0800

    Add docs for some model classes

--- src/slogo/model/Palette.java
+++ src/slogo/model/Palette.java
@@ -4,15 +4,26 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Color palette
+ *
+ * @author Jiyang Tang, Oliver Rodas
+ */
 public class Palette implements PaletTable, Serializable {
 
   private final Map<Integer, Color> colors = new HashMap<>();
   private static final Color DEFAULT_COLOR = new Color(0, 0, 0);
 
+  /**
+   * Set the color at given color idx
+   */
   public void setColor(int idx, Color color) {
     colors.put(idx, color);
   }
 
+  /**
+   * Get the color at given color idx
+   */
   public Color getColor(int idx) {
     return colors.getOrDefault(idx, DEFAULT_COLOR);
   }
